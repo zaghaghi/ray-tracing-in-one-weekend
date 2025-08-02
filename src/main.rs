9169489d05c7d8bed0c1e0d0141e4861b559be9d
Hamed Zaghaghi <hamed.zaghaghi@gmail.com>
2025-08-02 12:57:36 +0200

    feat: shaded with surface normals

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,24 @@ pub mod color;
 pub mod ray;
 pub mod vec3;
 
-fn hit_sphere(center: &Point3, radius: f64, ray: &Ray) -> bool {
+fn hit_sphere(center: &Point3, radius: f64, ray: &Ray) -> f64 {
     let oc = center - &ray.origin;
     let a = ray.direction.dot(&ray.direction);
     let b = ray.direction.dot(&oc) * -2.0;
     let c = oc.dot(&oc) - radius * radius;
     let d = b * b - 4.0 * a * c;
-    d >= 0.0
+    if d < 0.0 {
+        -1.0
+    } else {
+        (-b - d.sqrt()) / (2.0 * a)
+    }
 }
 
 fn ray_color(ray: &Ray) -> Color {
-    if hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, ray) {
-        return Color::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, ray);
+    if t > 0.0 {
+        let n = (ray.at(t) - DoubleVec3::new(0.0, 0.0, -1.0)).unit();
+        return Color::new(n.x + 1.0, n.y + 1.0, n.z + 1.0) * 0.5;
     }
     let unit_direction = ray.direction.clone().unit();
     let a = 0.5 * unit_direction.y + 1.0;
